77efbcd012d796c80ae9e2dc1f684bb4330b86ad
Mariano <mmontori@umich.edu>
2017-07-04 16:17:48 -0400

    View Controllers and Navigation

--- MakeSchoolNotes/Controllers/ListNotesTableViewController.swift
+++ MakeSchoolNotes/Controllers/ListNotesTableViewController.swift
@@ -13,5 +13,17 @@ class ListNotesTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+ 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
     
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "listNotesTableViewCell", for: indexPath) as! ListNotesTableViewCell
+        
+        cell.noteTitleLabel.text = "note's title"
+        cell.noteModificationTimeLabel.text = "note's modification time"
+        
+        return cell
+    }
 }
